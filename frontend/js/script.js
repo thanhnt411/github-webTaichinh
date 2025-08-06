ee553a1541b386370f94abd06730c178ea1b096e
thanhnt411 <darkphoenix4112006@gmail.com>
2025-08-06 11:27:28 +0700

    add banner

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -15,3 +15,21 @@ menuMobile.addEventListener("click",function() {
     }
 });
 
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    /*
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    */
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
