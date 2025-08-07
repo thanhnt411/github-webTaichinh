8853c50516a75abf78a4f8819fefa847ba7004fe
thanhnt411 <darkphoenix4112006@gmail.com>
2025-08-07 18:50:32 +0700

    Add full khung trang chủ

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -18,18 +18,16 @@ menuMobile.addEventListener("click",function() {
 var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
-    /*
+    loop: true,
+    
     autoplay: {
-        delay: 3000,
+        delay: 1000,
         disableOnInteraction: false,
     },
-    */
+    
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+   
 });
